af2364d8a851736d70273955bc28869fcfb3a611
Tim O'Malley <tomalley24@gmail.com>
2021-03-13 12:46:45 -0600

    started on fetches and zoomed in map

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -2,7 +2,23 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    center: { lat: 41.8781, lng: -87.6298 },
+    zoom: 14,
   });
 }
+
+// fetch('https://api.meetup.com/find/topics?query=tech&only=id,name')
+// .then(function (response) {
+//   if (response.ok) {
+//     console.log(response);
+//     response.json().then(function (data) {
+//       console.log(data);
+//       displayRepos(data, user);
+//     });
+//   } else {
+//     alert('Error: ' + response.statusText);
+//   }
+// })
+// .catch(function (error) {
+//   alert('Unable to connect to GitHub');
+// });
